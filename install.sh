3b27c83849695722be84fe8f2a13a079f0b2f4cd
Tin Lam <tinlam@gmail.com>
2024-07-15 22:12:58 -0500

    feat: add asdfrc
    Signed-off-by: Tin Lam <tinlam@gmail.com>

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ set -xeu
 /bin/bash -c " $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew packages
-brew install asdf fzf git eza coreutils yq jq vim
+brew install asdf fzf git eza coreutils yq jq nvim just 
 
-##
+# Install Other Stuff
